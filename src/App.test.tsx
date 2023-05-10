a7c8843db6ea23588428be02c88020a21aeaa51e
sam rubarema <samrubarema6@gmail.com>
2023-05-10 23:06:41 +0300

    added unit tests and refactored code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import { UserProvider } from "./utils/UserContext";
+import client from "./utils/ApolloClient";
+import App from "./App";
+import RouterConfig from "./routes/Router";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./routes/Router", () => jest.fn());
+describe("App", () => {
+
+  it("renders RouterConfig component", () => {
+    render(
+      <ApolloProvider client={client}>
+        <UserProvider>
+          <Router>
+            <App />
+          </Router>
+        </UserProvider>
+      </ApolloProvider>
+    );
+
+    expect(RouterConfig).toHaveBeenCalledTimes(1);
+  });
+
+  it("provides Apollo client through ApolloProvider", () => {
+    const ApolloProvider = jest.fn()
+    render(
+      <ApolloProvider client={client}>
+        <UserProvider>
+          <Router>
+            <App />
+          </Router>
+        </UserProvider>
+      </ApolloProvider>
+    );
+    expect(ApolloProvider).toHaveBeenCalledTimes(1);
+  });
+
+  it("provides UserContext through UserProvider", () => {
+    const UserProvider = jest.fn();
+    render(
+      <ApolloProvider client={client}>
+        <UserProvider>
+          <Router>
+            <App />
+          </Router>
+        </UserProvider>
+      </ApolloProvider>
+    );
+    expect(UserProvider).toHaveBeenCalled();
+  });
 });
+
--- src/utils/UserContext.tsx
+++ src/utils/UserContext.tsx
@@ -1,29 +1,38 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
+interface user {
+  name: string;
+  height: number;
+  mass: number;
+  gender: string;
+  homeworld: string;
+}
 interface UserContextType {
-  users: any[];
-  setUsers: React.Dispatch<React.SetStateAction<string[]>>;
+  users: user[];
+  setUsers: React.Dispatch<React.SetStateAction<{}[]>>;
   currentPage: number;
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>>
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
 export const UserContext = createContext<UserContextType>({
   users: [],
   setUsers: () => {},
   currentPage: 1,
-  setCurrentPage: () => {}
+  setCurrentPage: () => {},
 });
 
 interface Props {
-    children: React.ReactNode;
-  }
+  children: React.ReactNode;
+}
 
 export const UserProvider: React.FC<Props> = ({ children }) => {
-  const [users, setUsers] = useState<string[]>([]);
-  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [users, setUsers] = useState<any>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   return (
-    <UserContext.Provider value={{ users, setUsers, currentPage, setCurrentPage  }}>
+    <UserContext.Provider
+      value={{ users, setUsers, currentPage, setCurrentPage }}
+    >
       {children}
     </UserContext.Provider>
   );
